74cfb652b2f4163350fc57c7e858b226f3e7e84b
Hiroki Osame <hiroki@weebly.com>
2019-03-08 23:03:06 -0500

    log

--- npm_postversion.sh
+++ npm_postversion.sh
@@ -2,15 +2,19 @@
 
 set -e
 
-# Delete tag just created
-git tag -d "v$npm_package_version";
 
 # Build files
+echo 'Building';
 node ./buildFile.js;
 git add built.js;
 
 # Build Commit
+echo 'Committing build';
 git commit -m "$npm_package_version-built";
+
+# Delete tag just created
+echo 'Rewriting tag';
+git tag -d "v$npm_package_version";
 git tag -a "$npm_config_tag_version_prefix$npm_package_version" -m $npm_package_version;
 
 # git push origin "v$npm_package_version";
@@ -18,5 +22,6 @@ git tag -a "$npm_config_tag_version_prefix$npm_package_version" -m $npm_package_
 # Revert Build commit
 # git push origin master;
 # git reset --soft HEAD~1;
+echo 'Reverting';
 git revert HEAD~1;
 
