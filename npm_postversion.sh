9a0903c084761a1c2088fabf8830b55368fc324b
Hiroki Osame <hiroki@weebly.com>
2019-03-08 23:18:51 -0500

    release

--- npm_postversion.sh
+++ npm_postversion.sh
@@ -2,27 +2,18 @@
 
 set -e
 
-echo "1. Deleting tag";
-git tag -d "v$npm_package_version";
-
 # Build files
-echo "2. Building";
 node ./buildFile.js;
 git add built.js;
 
 # Build Commit
-echo "3. Committing build: $npm_package_version-built";
 git commit -m "$npm_package_version-built";
 
 # Delete tag just created
-echo "4. Retagging";
-git tag -a "$npm_config_tag_version_prefix$npm_package_version" -m $npm_package_version;
-
-# git push origin "v$npm_package_version";
+TAGNAME="$npm_config_tag_version_prefix$npm_package_version"
+git tag -d $TAGNAME;
+git tag $TAGNAME -am $npm_package_version;
 
 # Revert Build commit
-# git push origin master;
-# git reset --soft HEAD~1;
-echo "5. Reverting";
-git revert HEAD~1;
-
+git revert --no-commit HEAD~1;
+g commit -m "$TAGNAME Release";
